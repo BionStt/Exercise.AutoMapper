b6d9a3726b8aa42ff73d3011f7111beb45133846
Mikus Miroslav <miroslav.mikus@outlook.com>
2018-05-16 20:55:39 +0200

    Add composite example

--- Exercise.AutoMapper.Composite/Program.cs
+++ Exercise.AutoMapper.Composite/Program.cs
@@ -1,3 +1,6 @@
+using AutoMapper;
+using Exercise.AutoMapper.Composite.Auto_Mapper;
+using Exercise.AutoMapper.Composite.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +13,21 @@ namespace Exercise.AutoMapper.Composite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            IConfigurationProvider provider = new MapperConfiguration(conf => conf.AddProfile(new MapperProfile()));
+
+            IMapper mapper = provider.CreateMapper();
+
+            IEnumerable<UserSource> user = FakeStorage.GetUsers();
+
+            IList<UserTarget> usertarget = mapper.Map<IList<UserTarget>>(user);
+
+            foreach (var item in usertarget)
+            {
+                Console.WriteLine($"element nr.: {usertarget.IndexOf(item)}");
+                Console.WriteLine(item.Print());
+            }
+
+            Console.ReadLine();
         }
     }
 }
